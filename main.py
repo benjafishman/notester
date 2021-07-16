32e5c186970e75ddfde2b839ec0d60512afc9214
bentzion <benjafishman@gmail.com>
2021-07-16 08:52:18 +0300

    total overhaul of main eith simplified hotkey code, adding note model stuff

--- main.py
+++ main.py
@@ -1,111 +1,62 @@
-"""
-בס"ד
-Bentzion Fishman
-Bein Hazmanim 2021
-"""
-
-from pynput.keyboard import Controller
 from pynput import keyboard
 import threading
-import time
+import note
+from pynput.keyboard import Controller
 
-# keeps track of the type of input a user is doing, should probably change name of the var for more clarity
+#global variable
 glob_level = 0
-
-# The key combination to check
-COMBINATIONS = [
-    {keyboard.Key.shift, keyboard.KeyCode(char='X')},
-    {keyboard.Key.shift, keyboard.KeyCode(char='x')},
-]
-
-# The currently active modifiers
-current = set()
 
 # the keyboard controller specifically to simulate pressing buttons
 kb = Controller()
+from pynput.keyboard import Controller
 
-'''
-- Execute function is execute when a user press ctrl^x (although right now it's only x^ctrl?)
-- This takes the global variable glob_level and increases it by 1 then mods it by 2 since we have two options
-the mod function acts like a toggle between 2 values
-- then sets the new glob_level value which will be used by the getUserInput() function which is on a constant loop
-'''
-
-
-def execute():
+def function_1():
     global glob_level
-    temp = (glob_level + 1) % 2
-    glob_level = temp
+    print("function 1")
+    glob_level = 0
+    end_execute()
 
+
+def function_2():
+    print("function 2")
+    global glob_level
+    glob_level = 1
+    end_execute()
+
+
+def end_execute():
     # simulate pressing enter so the user input type will change
     # TODO: decide what should be done with anything user has input so far and now they are changing input type
     kb.press(keyboard.Key.enter)
     kb.release(keyboard.Key.enter)
 
-
-'''
-Function on_press checks if any of the HotKeys have been pressed 
-then calls the execute function if true
-'''
-
-
-def on_press(key):
-    # print([COMBO for COMBO in COMBINATIONS])
-    if any([key in COMBO for COMBO in COMBINATIONS]):
-        current.add(key)
-        if any(all(k in current for k in COMBO) for COMBO in COMBINATIONS):
-            execute()
+def start():
+    c = input("Enter title of note: ")
+    # TODO: check for any notes with the title,
+    # but for now just create new note
+    return note.Note(c)
 
 
-def on_release(key):
-    if any([key in COMBO for COMBO in COMBINATIONS]):
-        print(f'the key is: {key}')
-        print(f'current is {current}')
-        try:
-            current.remove(key)
-        except:
-            #TODO: do something more productive with this error ,uhh, like fix it.
-            print("ERROR ERROR ERROR")
-
-
-def get_user_input():
+def get_user_input(header=None):
     d = {
-        0: "top_level content(*):",
-        1: "sub_level content(-):"
+        0: "section header(*):",
+        1: f'[{header}]| input content:',
     }
 
     while True:
-        c = input(f"Enter {d[glob_level]}")
+        c = input(f"{d[glob_level]}")
         print(f'input is: {c}')
 
 
 def main():
     thread2 = threading.Thread(target=get_user_input, args=())
     thread2.start()
-    with keyboard.Listener(on_press=on_press,
-                           on_release=on_release)as listener:
-        listener.join()
+    with keyboard.GlobalHotKeys({
+        '<shift>+R': function_1,
+        '<shift>+T': function_2}) as h:
+        h.join()
 
 
+
+my_note = start()
 main()
-
-'''
-if __name__ == '__main__':
-    # keyboard.write("GEEKS FOR GEEKS\n")
-    # Collect events until released
-    # keyboard.on_press_key('f', top_level)
-    with Listener(
-            on_press=on_press,
-            on_release=on_release) as listener:
-        listener.join()
-
-What do we need to do: 
-1. get the title of the note
-2. get the content 
-    4 main markers for content
-        0. ? major questions on the sugya
-        1. * overarching idea
-        2. # listing within an overarching idea
-        3  - subinformation within the overarching idea and depending on number of 
-             dashes is related to how nested the current idea is 
-'''
--- note.py
+++ note.py
@@ -1,15 +1,85 @@
+import datetime
+
+
 class Note:
-    """This is a docstring. I have created a new class"""
-    title = ""
-    content = {}
+    """A class to build a JSON string of a note"""
+    date_modified = None
+    json_note = {}
+    edit_pointer = None
+    """
+    stub content:
+    First mishnah in second perek of avos
+        - "rebbi says whats the straight path a person should follow in this world"
+        - anything that brings him glory and makes others glorify him
+        - one should be alacritous to do easy mitzvos as if they were hard ones since we don't know the reward for mitzvos
+        - cheshbon the loss of doing a mitzvoh versus the gain of not doing it and the gain of doing and aveira versus the loss of not doing it
+    
+    note = {
+	"meta_data": {
+		"title": "Pirkei Avos",
+		"date_created": "todays_date"
+	},
+	"content": {
+		"First mishnah": {
+			"0": "rebbi says whats the staright path a person should follow in this world",
+			"1": "anything that brings him glory and makes others glorify him",
+			"2": "one should be alacritous to do easy mitzvos as if they were hard ones since we dont know the reward for mitzvos",
+			"3": "cheshbon the loss of doing a mitzvoh versus the gain of not doing it and the gain of doing and aveira versus the loss of not doing it"
+		},
+		"Second mishnah": {
+			"0": "raban gamliel son of Rebbi yehudah hanasi says torah is beatiful with derech eretz because together they make one forget sin",
+			"1": "torah without work will in the end be nullified and lead to sin",
+			"3": "anyone who is involved with the tzibur should do so lshem shamayim",
+			"4": "because their zches avos will assist them and their righteousness stands forever",
+			"5": "and as for the leader he will get rewarded as if he did it himself"
+		}
+	}
+}
+
+    """
     references = None  # perhaps this could be a dictionary of other related notes
 
-    def __init__(self, title):
-        self.title = title
+    def __init__(self, title, content=None):
+        self.edit_pointer = 0
 
-    def set_content(self, content):
-        self.content = content
+        # set meta data content
+        self.json_note['meta_data'] = {
+            'title': title,
+            'date_created': datetime.date.today()
+        }
+
+    def add_section_with_content(self, key, content):
+        self.json_note[key] = content
+
+    def add_content(self, position, content):
+        self.json_note[position] = content
+
+    def traverse_note(self):
+        for k, v in self.json_note.iteritems():
+            print(k, v)
+
+    def get_sections(self):
+        """Returns list of sections relative to user's depth in the note"""
+        keys = []
+        for k, v in self.json_note.items():
+            print(f'key is: {k}, v is: {v}')
+            keys.append(k)
+        return keys
+
+    def add_or_edit_by_address(self, keys, val):
+        """
+        Nested_set takes a dictionary with a list of keys whose order parallels how the dictionary is nested and
+        then assigns the value to that location. This will can add a new dictionary or update an existing entry in a
+        dictionary
+        """
+        dic = self.json_note
+        for key in keys[:-1]:
+            print(f'start dic: {dic}')
+            print(f'key is {key}')
+            dic = dic.setdefault(key, {})
+            print(f'end dic: {dic}')
+        dic[keys[-1]] = val
+
 
     def print(self):
-        print(f'Note title, {self.title}')
-
+        print(self.json_note)
